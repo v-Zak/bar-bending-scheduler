fedb5752a4ea5d53d27a6f817d75b1a348c82faa
v-Zak <77330725+v-Zak@users.noreply.github.com>
2023-06-07 22:30:44 +0100

    added gui template, need to fix print calls

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-from DxfHandler import DxfHandler 
-from LabelParser import LabelParser
-from BarHandler import BarHandler
-from CsvHandler import CsvHandler
 from BarBendingScheduler import BarBendingScheduler
 
-if __name__ == "__main__":
+import kivy
+kivy.require('2.2.0')
 
-    #dxf_path = input("Enter a DXF File Path (include .dxf): ")
-    dxf_path = "tests/dxf_samples/text_test_3.dxf"
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
-    bbs = BarBendingScheduler()
-    bbs.createBarQuantitiesCsv(dxf_path)
+class RebarReader(App):
+    def build(self, **kwargs):
+        layout = BoxLayout(padding=10, orientation="vertical")
+        self.path_label = Label(
+            text = "DXF File Location:",
+            size_hint = (1, None),
+            font_size=15,
+            height = 20
+        )
+
+        self.path = TextInput(
+            multiline=False, 
+            readonly=False, halign="right", 
+            font_size=15, 
+            size_hint = (1, None), 
+            height = 50,
+            text = "tests/dxf_samples/text_test_3.dxf"
+            )
+        
+        self.create_btn = Button(
+            text="Create", size_hint = (0.5, None), height = 40, pos_hint = ({'x': .25})
+            )
+        
+        self.create_btn.bind(on_press = self.create) 
+
+        self.console_label = Label(
+            text = "Console:",
+            size_hint = (1, None),
+            font_size=15,
+            height = 20
+        )
+
+        self.console = TextInput(
+            multiline=True, readonly=True, halign="right", font_size=15, size_hint = (1, 0.6)
+        )
+        
+
+        
+        layout.add_widget(self.path_label)
+        layout.add_widget(self.path)
+        layout.add_widget(self.create_btn)
+        layout.add_widget(self.console_label)
+        layout.add_widget(self.console)
+        return layout
+    
+    def create(self, dxf_path):
+        dxf_path = self.path.text
+        bbs = BarBendingScheduler()
+        bbs.createBarQuantitiesCsv(dxf_path)
+
+if __name__ == '__main__':
+    RebarReader().run()
+
+
 
 
 
